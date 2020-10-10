f406fd96d5c95aa3ac08055e0f07006410d948c1
Ashutosh Samal <ashutosh.samal9@gmail.com>
2020-10-10 22:27:04 +0100

    input

--- inp.py
+++ inp.py
@@ -1,25 +1,7 @@
 # create rows and colums from file
 # take in input cols, rows
 # output array with either true or false
-<<<<<<< HEAD
-=======
- 
 
-def input(inp):
-    try:
-    cells = [] 
-
-
-     # read in file 
-    with open(inp) as f:
-        cells = f.read()
-
-    else:
-        r = int(input('rows'))
-        c = int(input('columns'))
-    
-
->>>>>>> 7a97fdd584d04aa46901a5b33778473e529c3283
 
 def input(filename):
     cells = []
