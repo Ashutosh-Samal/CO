bad2e7d619034688b6f1467d47fbcc1c0a9813b5
Ashutosh Samal <ashutosh.samal9@gmail.com>
2020-10-10 18:22:13 +0100

    Master branch

---
+++ main.py
@@ -0,0 +1,5 @@
+import inp, nextgen, check_if_three, time, sys
+
+if __name__ == "__main__":
+    arr = inp.inp(sys.args[1])
+    
---
+++ nextgen.py
@@ -0,0 +1,20 @@
+def nextgen(arr, i, j):
+    if arr[i][j] == False:
+        return False
+    r = len(arr)
+    c = len(arr[0])
+    count = 0
+    neighbours = []
+    for a in range(i-1,i+1):
+        for b in range(j-1,j+1):
+            if a != i and b != j:
+                if a >= 0 and a <= r and b >= 0 and b <= c:
+                    neighbours.append([a,b])
+
+    for indices in neighbours:
+        x, y = indices
+        if arr[x,y] == True:
+            count+=1
+
+    if count in range(2,4):
+        return True
