af9e55aefd49598d447573a028c0475fad30a547
dog1289 <gunterzwanzig@outlook.com>
2020-10-10 17:39:06 +0100

    input

---
+++ inp.py
@@ -0,0 +1,22 @@
+# filename = inp  
+# create rows and colums from file 
+# take in input cols, rows
+# output array with either true or false
+ 
+
+def input(inp):
+    try:
+    import numpy as np
+    cells = [] 
+
+
+     # read in file 
+    with open(inp) as f:
+        cells = f.read()
+
+    else:
+        r = int(input('rows'))
+        c = int(input('columns'))
+    
+
+
